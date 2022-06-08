931c9d442fa61c1abeb30c94752be5f70da249f6
Luis <lvas248@gmail.com>
2022-06-08 17:43:55 -0400

    commit tree 5f1763f62b93fc137c0384bc1ba8162f28e86be7

--- index.js
+++ index.js
@@ -1,1 +1,27 @@
 // Add your code here
+function submitData(name, email){
+
+    return fetch("http://localhost:3000/users", {
+        method: 'POST',
+        headers: {
+            "Content-Type":"application/json",
+            "Accept":"application/json"
+        },
+        body: JSON.stringify({
+            name: `${name}`,
+            email:`${email}`            
+        })
+    })
+    .then(res => res.json())
+    .then(data => {
+        let p = document.createElement('p')
+        p.textContent = data.id
+        document.querySelector('body').append(p)
+    })
+    .catch(function(error){
+        alert(error)
+        let errorMessage = document.createElement("h1")
+        errorMessage.textContent = error.message
+        document.querySelector("body").append(errorMessage)
+    })
+}    
